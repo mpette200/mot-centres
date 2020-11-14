73b7420221b7458ee061b06ae85c003529d1abd3
mark.petterson <mpette200@gmail.com>
2020-11-14 17:45:21 +0000

    coordinate lookup done

--- src/postcodes.js
+++ src/postcodes.js
@@ -1,36 +1,106 @@
-var postcodeLib = function postcode() {
-  var public = {};
+'use strict';
+
+var postcodeData = {};
+
+// place to hold result data
+postcodeData.PATH_P = '';
+postcodeData.PATH_MOT = '';
+postcodeData.ERR_MSG = 'Postcode Not Found';
+postcodeData.ERR_LOAD = 'Error loading mot stations data';
+postcodeData.coords = null;
+postcodeData.stations = null;
+
+/**
+* Constructor creates a new pair of coordinates
+* this.east and this.north
+*/
+function Coordinates(easting, northing) {
+  this.east = easting;
+  this.north = northing;
+}
+
+/**
+ * Calculates the distance between 2 pairs of coordinates
+ * returns distance in miles
+ */
+function getDistance(coord1, coord2) {
+  var diffE = coord1.east - coord2.east;
+  var diffN = coord1.north - coord2.north;
   
-  public.getPrefix = getPrefix;
-  public.coordinates = coordinates;
-  public.parseCoordinates = parseCoordinates;
+  var distKm = (diffE**2 + diffN**2) ** 0.5;
+  var distanceMiles = distKm / 1000 / 1.6;
   
-  /**
-  * Helper method to get the 1st 2 characters of the postcode
-  */
-  function getPrefix(txt) {
-    return txt.trim().substring(0, 2).toLowerCase();
+  return distanceMiles;
+}
+
+/**
+* Get the coordinates of the postcode by fetching relevant json file
+* json file has the 1st letter part of the postcode
+* eg. EC2M 5BB -> ec.json
+* 
+* Always returns null, action initiated by callback passed as parameter
+*/
+function getCoordinates(txt, callback) {
+  // any letter 1 or 2 times at beginning of string
+  var regex = /^[a-z]{1,2}/
+  var prefix = txt.trim().toLowerCase().match(regex);
+  if (prefix === null) {
+    postcodeData.coords = postcodeData.ERR_MSG;
+    callback();
+    return null;
   }
+
+  var url = postcodeData.PATH_P + prefix + '.json';
+  var req = new XMLHttpRequest();
+  req.responseType = 'json';
+  req.open('GET', url, true);
+  req.send();
   
-  /**
-  * Constructor creates a new pair of coordinates
-  * this.east and this.north
-  */
-  function coordinates(easting, northing) {
-    this.east = easting;
-    this.north = northing;
-  }
+  req.onreadystatechange = function () {
+    if (req.readyState == 4) {
+      if (req.status == 200) {
+        
+        var postcodeLoc = req.response;
+        var query = txt.trim().toUpperCase();
+        var coords = postcodeLoc[query];
+
+        postcodeData.coords = new Coordinates(coords[0], coords[1]);
+        callback();
+      }
+      else {
+        postcodeData.coords = postcodeData.ERR_MSG;
+        callback();
+      }
+    }
+  };
+
+  return null;
+}
+
+/**
+ * Loads list of MOT stations
+ * 
+ * Always returns null, action initiated by callback passed as parameter
+ */
+function getMotStations(coords, callback) {
+  var url = postcodeData.PATH_MOT;
+  var req = new XMLHttpRequest();
+  req.responseType = 'json';
+  req.open('GET', url, true);
+  req.send();
   
-  /**
-  * Get the coordinates of the postcode by parsing text
-  * text is formatted as a space separated file
-  * the offsets are:  0          10       20
-  * the data is:      XXXX XXX   123456   123456
-  */
-  function parseCoordinates(txt) {
-    out = new coordinates(0, 0);
-    return out;
-  }
-  
-  return public;
-}();
+  req.onreadystatechange = function () {
+    if (req.readyState == 4) {
+      if (req.status == 200) {
+        postcodeData.stations = req.response;
+        callback();
+      }
+      else {
+        postcodeData.stations = postcodeData.ERR_LOAD;
+        callback();
+      }
+    }
+  };
+
+  return null;
+}
